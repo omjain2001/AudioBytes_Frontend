af5c7ff931e9f9dda4a5a40d71eb9da8faa11d6c
siddheshdhainje2001 <siddheshdhainje2001@gmail.com>
2022-10-29 16:28:04 +0530

    formik added

--- client/src/components/AudioInput.js
+++ client/src/components/AudioInput.js
@@ -1,42 +1,70 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Progress from "./Progress";
-import {useFormik} from "formik"
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 const AudioInput = () => {
-  const [file, setFile] = React.useState("");
+  const fileInputRef = useRef();
+  const [uploadPercentange, setUploadPercentange] = useState(0);
 
   const formik = useFormik({
     initialValues: {
-      audioFile: null
+      audioFile: "",
     },
-    onSubmit : (values) => {
-      console.log("Formik values", values)
-    }
-  })
+    validationSchema: Yup.object({
+      audioFile: Yup.string().required("Audio file is required"),
+    }),
+    onSubmit: async (values, helpers) => {
+      console.log("Formik values", values);
+      try {
+        const formData = new FormData();
 
-  const handleChange = (e) => {
-    setFile(e.target.files[0]);
-    formik.setFieldValue("audioFile", file)
-  }
+        formData.append("file", values.audioFile);
+        console.log("formData data", formData);
+        const res = await axios.post("/upload_file", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (progressEvent) => {
+            setUploadPercentange(
+              parseInt(
+                Math.round((progressEvent.loaded * 100) / progressEvent.total)
+              )
+            );
+            setTimeout(() => setUploadPercentange(0), 2000);
+          },
+        });
+        fileInputRef.current.value = "";
+        helpers.resetForm();
+      } catch (error) {
+        console.log("Error from client side", error);
+      }
+    },
+  });
 
   return (
     <div className="container mb-4">
       <form onSubmit={formik.handleSubmit}>
-      <div class="input-group mb-3">
-        <input
-          type="file"
-          class="form-control"
-          id="inputGroupFile02"
-          onClick={handleChange}
-        />
-        <label class="input-group-text" for="inputGroupFile02">
-          Upload
-        </label>
-      </div>
-      <Progress percentage={0} />
-      <button type="submit" class="btn btn-primary mt-4 w-100">
-        Submit
-      </button>
+        <div class="input-group">
+          <input
+            type="file"
+            className="form-control"
+            ref={fileInputRef}
+            onChange={(e) => {
+              formik.setFieldValue("audioFile", e.target.files[0]);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <small className="text-danger form-text">
+            {formik.touched.audioFile && formik.errors.audioFile}
+          </small>
+        </div>
+        <Progress percentage={uploadPercentange} />
+        <button type="submit" className="btn btn-primary mt-4 w-100">
+          Submit
+        </button>
       </form>
     </div>
   );
