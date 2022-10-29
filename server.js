10d53f80ac1839bede8b666ffc3553a822620830
siddheshdhainje2001 <siddheshdhainje2001@gmail.com>
2022-10-29 15:38:36 +0530

    file compression added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const multer = require("multer");
+const AdmZip = require("adm-zip");
+const fs = require("fs");
 
 var app = express();
 
@@ -9,17 +11,24 @@ const storage = multer.diskStorage({
     console.log("filename");
     cb(null,Date.now() +"---"+ file.originalname);
   },
-  destination: function (req, file, cb) {
-    console.log("storage");
-    cb(null, "./uploads");
-  },
+  // destination: function (req, file, cb) {
+  //   console.log("storage");
+  //   cb(null, "./uploads");
+  // },
 });
 
 const upload = multer({ storage });
 
-app.post('/upload_files', upload.single('file'), (req, res) => {
-    console.log(req.body)
+app.post('/upload_file', upload.single('file'), (req, res) => {
     console.log(req.file)
+    const zip = new AdmZip();
+
+    zip.addLocalFile(req.file.path);
+
+    var outputPath ="./uploads/" + Date.now() + "output.zip"
+    
+    fs.writeFileSync(outputPath, zip.toBuffer());
+
     res.send({ message: 'Successfully uploaded files' })
   })
 
