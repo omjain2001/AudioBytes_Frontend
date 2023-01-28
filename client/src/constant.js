46a88e55f6b65daa2a75e2828654310e3db69590
Om Jain <omjain2606@gmail.com>
2023-01-28 21:39:07 +0530

    Changed UI

---
+++ client/src/constant.js
@@ -0,0 +1,4 @@
+export const COLORS = {
+  PRIMARY: "#0c4a6e",
+  SECONDARY: "#0284c7",
+};
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,45 @@
-import React from 'react'
-import AudioInput from '../components/AudioInput'
+import React from "react";
+import AudioInput from "../components/AudioInput";
+import { COLORS } from "../constant";
 
-const Home = () =>{
+const Home = () => {
   return (
-    <div style={{width : "50rem"}} className="container mt-4">
-      <h4 className="display-4 text-center mb-5">
-        <i className="fab fa-react" /> Audio File Upload
-      </h4>
-      <AudioInput />
+    <div className="container vh-100 py-5">
+      <h1
+        className="text-center"
+        style={{ fontWeight: 600, letterSpacing: 1, color: COLORS.PRIMARY }}
+      >
+        AUDIOBYTES
+      </h1>
+      <div
+        className="card w-35 home py-4 px-4"
+        style={{ boxShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px" }}
+      >
+        <div className="card-body">
+          <h3
+            className="card-title text-center"
+            style={{
+              fontWeight: 600,
+              color: COLORS.SECONDARY,
+            }}
+          >
+            <i className="fas fa-cloud-arrow-up" />
+            &nbsp; Upload your file
+          </h3>
+          <h6
+            className="card-subtitle text-center"
+            style={{ fontWeight: 400, color: "#b3bdcc" }}
+          >
+            Upload only .mp3 and .wav files
+          </h6>
+          <div className="card-text">
+            {" "}
+            <AudioInput />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
